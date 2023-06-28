5437034b7a38d27f6590e8262e65fe316f81a45c
李元琦 <yuan.chi@program.com.tw>
2023-06-28 11:29:28 +0800

    chore: add Eslint & Prettierc

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+// copy
+module.exports = {
+  printWidth: 120,
+  semi: false, // 行尾部需分號
+  singleQuote: true, // 使用單引號
+  tabWidth: 2,
+  trailingComma: 'es5', // 尾隨逗號
+  endOfLine: 'lf', // 換行使用 lf
+  arrowParens: 'avoid',
+  useTabs: false,
+  jsxSingleQuote: true, // jsx 使用單引號
+  bracketSpacing: true, // 大括號內的首尾需空格
+  bracketLine: false,
+  arrowParens: 'avoid',
+}
