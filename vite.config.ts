fd64b5e9a37407fe365f9ddde755a8e5d5d2de63
李元琦 <yuan.chi@program.com.tw>
2023-06-28 15:36:25 +0800

    feat: add vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+    },
+  },
   server: {
     open: true,
     port: 4000,
