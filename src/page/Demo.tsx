eadc52a6efd1274db4569ee0516a5ac3e015f33a
李元琦 <yuan.chi@program.com.tw>
2024-05-23 17:16:27 +0800

    feat: 顯示 OSM 地圖

--- src/page/Demo.tsx
+++ src/page/Demo.tsx
@@ -1,3 +1,26 @@
+import { LatLngExpression } from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
 export default function Demo() {
-  return <>Demo Page</>
+  const position: LatLngExpression = [25.0408578889, 121.567904444] // 台北某地標
+  const zoom = 15
+
+  return (
+    <>
+      <p>Demo Page</p>
+      <p>圖示</p>
+      <div id='map' style={{ height: '50vh', width: '80vw' }}>
+        <MapContainer center={position} zoom={zoom} style={{ height: '100%', minHeight: '100%' }}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png'
+          />
+          <Marker position={position}>
+            <Popup>test popup</Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </>
+  )
 }
