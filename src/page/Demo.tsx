2fe356e318f203730bd4d7e8d6556386937c3bc0
李元琦 <yuan.chi@program.com.tw>
2024-05-24 10:13:41 +0800

    feat: 點擊地圖移動到使用者當前位置

--- src/page/Demo.tsx
+++ src/page/Demo.tsx
@@ -1,10 +1,31 @@
 import { LatLngExpression } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { useState } from 'react'
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet'
+
+// TODO: 顯示不同情境的站點
 
 export default function Demo() {
-  const position: LatLngExpression = [25.0408578889, 121.567904444] // 台北某地標
+  const [position, setPosition] = useState<LatLngExpression>([25.02605, 121.5436]) // 台北某地標
   const zoom = 15
+
+  const LocationMarker = () => {
+    const map = useMapEvents({
+      click: () => {
+        map.locate()
+      },
+      locationfound: location => {
+        setPosition(location.latlng)
+        map.flyTo([location.latlng.lat, location.latlng.lng], zoom)
+      },
+    })
+
+    return (
+      <Marker position={position}>
+        <Popup>You are here.</Popup>
+      </Marker>
+    )
+  }
 
   return (
     <>
@@ -16,9 +37,7 @@ export default function Demo() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url='https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png'
           />
-          <Marker position={position}>
-            <Popup>test popup</Popup>
-          </Marker>
+          <LocationMarker />
         </MapContainer>
       </div>
     </>
